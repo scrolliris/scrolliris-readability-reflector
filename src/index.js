448fed6d7922faf3015414053c213fc0a5a236df
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2017-12-14 04:17:06 +0900

    Fix minimap rendering

--- src/index.js
+++ src/index.js
@@ -54,10 +54,10 @@ class Widget {
     let src = {};
 
     // -- minimap
-    src.js = settings.minimap.js || scriptSrc.replace(
-      reflectorJSRegex, '-minimap$2.js$3').toString();
-    src.css = settings.minimap.css || scriptSrc.replace(
-      reflectorJSRegex, '-minimap$2.css$3').toString();
+    src.js = settings.hasOwnProperty('minimap') ? settings.minimap.js :
+      scriptSrc.replace(reflectorJSRegex, '-minimap$2.js$3').toString();
+    src.css = settings.hasOwnProperty('minimap') ? settings.minimap.css :
+      scriptSrc.replace(reflectorJSRegex, '-minimap$2.css$3').toString();
 
     if (src.js === '' || src.css === '') {
       console.error('widget source (js|css) is missing');
